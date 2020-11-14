676c4da805731bf7b5d23e8c9f9b5d99224e71ce
Théophile Tesseraud <theophiletesseraud@linux.home>
2020-11-14 16:24:25 +0100

    ajout des instances difficiles et génération dans script_sac_a_dos.py + début des analyses sur les instances faciles

--- generation_instance.py
+++ generation_instance.py
@@ -6,42 +6,54 @@ def uniforme(a,b):
     return a+(b-a)*random()
 
 
-def generation_une_instance_difficile(taille):
-    iteration=[i+1 for i in range(taille)]
+def generation_une_instance(nombre_objet,info_taille,info_duree):
+    iteration=[i+1 for i in range(nombre_objet)]
     dict_object=dict()
-    mu = 4.0
-    sigma = 1.0
     for i in iteration:
         video=list()
-        duree=uniforme(0,100)
-        taille=np.random.randn(1) * sigma + mu
-        taille=taille[0]
+        if info_taille=="high":
+            mu = 4.0
+            sigma = 0.5
+            taille=np.random.randn(1) * sigma + mu
+            taille=taille[0]
+        elif info_taille=="light":
+            mu= 1.0
+            sigma=0.5
+            taille=np.random.randn(1) * sigma + mu
+            taille=taille[0]
+        else:
+            taille=uniforme(0,5)
+            
+        if info_duree=="high":
+            mu = 80.0
+            sigma = 10.0
+            duree=np.random.randn(1) * sigma + mu      
+            duree=duree[0]      
+        elif info_duree=="light":
+            mu= 30.0
+            sigma=10.0
+            duree=np.random.randn(1) * sigma + mu
+            duree=duree[0]
+        else:
+            duree=uniforme(0,100)
         num_object="objet "+str(i)
         dict_object[num_object]=[duree, taille]
     return dict_object
 
 
-
-def generation_une_instance(taille):
-    iteration=[i+1 for i in range(taille)]
-    dict_object=dict()
-    for i in iteration:
-        video=list()
-        duree=uniforme(0,100)
-        taille=uniforme(0,5)
-        num_object="objet "+str(i)
-        dict_object[num_object]=[duree, taille]
-    return dict_object
-
-
-
-
-
-def generation_instances(taille,nb_instance):
+def generation_plusieurs_instances(nb_objet,nb_instance,info_taille,info_duree):
     iteration=[i+1 for i in range(nb_instance)]
     dict_instance=dict()
     for i in iteration:
-        instance_seul=generation_une_instance(taille)
+        instance_seul=generation_une_instance(nb_objet,info_taille,info_duree)
         num_instance="instance"+str(i)
         dict_instance[num_instance]=instance_seul
     return dict_instance
+
+
+generation_plusieurs_instances(nb_objet=1000,nb_instance=100,info_taille="high",info_duree="light")
+generation_plusieurs_instances(nb_objet=1000,nb_instance=100,info_taille="high",info_duree="high")
+generation_plusieurs_instances(nb_objet=1000,nb_instance=100,info_taille="light",info_duree="light")
+generation_plusieurs_instances(nb_objet=1000,nb_instance=100,info_taille="light",info_duree="hight")
+
+
--- script_sac_a_dos.py
+++ script_sac_a_dos.py
@@ -1,34 +1,53 @@
 from algo import *
+from generation_instance import *
 
 
 
 '''
-instances=generation_instances(100,10)
+#génération des instances
+
+instances_normal=generation_plusieurs_instances(nb_objet=100,nb_instance=10,info_taille="normal",info_duree="normal")
+with open('instances_normal.json', 'w', encoding='utf-8') as f:
+    json.dump(instances_normal, f, indent=4)
 
 
-with open('Test.json', 'w', encoding='utf-8') as f:
-    json.dump(instances, f, indent=4)
+instances_TH_DL=generation_plusieurs_instances(nb_objet=100,nb_instance=10,info_taille="high",info_duree="light")
+with open('instances_TH_DL.json', 'w', encoding='utf-8') as f:
+    json.dump(instances_TH_DL, f, indent=4)
+
+instances_TH_DH=generation_plusieurs_instances(nb_objet=100,nb_instance=10,info_taille="high",info_duree="high")
+with open('instances_TH_DH.json', 'w', encoding='utf-8') as f:
+    json.dump(instances_TH_DH, f, indent=4)
+
+instances_TL_DL=generation_plusieurs_instances(nb_objet=100,nb_instance=10,info_taille="light",info_duree="light")
+with open('instances_TL_DL.json', 'w', encoding='utf-8') as f:
+    json.dump(instances_TL_DL, f, indent=4)
+
+instances_TL_DH=generation_plusieurs_instances(nb_objet=100,nb_instance=10,info_taille="light",info_duree="hight")
+with open('instances_TL_DH.json', 'w', encoding='utf-8') as f:
+    json.dump(instances_TL_DH, f, indent=4)
 '''
 
 
+#Regardons d'abord pour les instances normales
+
+with open('instances_normal.json') as json_data:
+    dict_instances_normal= json.load(json_data)
 
 
-videos = [('Video1', 114, 4.57), ('Video2', 32, 0.630),('Video3', 20, 1.65), ('Video4', 4, 0.085),('Video5', 18, 2.15), ('Video6', 80, 2.71),('Video7', 5, 0.320)]
+resultat_brut=list()
+resultat_duree=list()
+resultat_taille=list()
+resultat_rapport=list()
 
-
-
-choix = force_brut(videos, 5)
-duree_totale = choix[1]
-choix_fichiers = [fichier[0] for fichier in choix[0]]
-print(choix_fichiers, duree_totale)
-
-'''
-with open('Test.json') as json_data:
-    dict_instances= json.load(json_data)
-
-
-for key,value in dict_instances.items():
+for key,value in dict_instances_normal.items():
     liste_instance=dict_instance_to_liste_instance(value)
-    liste_resultat =glouton(liste_instance, 5, duree)
-    print(len(liste_resultat[0]),liste_resultat[1])
-'''
+    print("résultat pour l'",key)
+    liste_resultat_brut=force_brut(liste_instance, 20)
+    print("résultat brut", len(liste_resultat_brut[0]),liste_resultat_brut[1])
+    liste_resultat_duree =glouton(liste_instance, 20, duree)
+    print("résultat pour la duree",len(liste_resultat_duree[0]),liste_resultat_duree[1])
+    liste_resultat_taille =glouton(liste_instance, 20, taille)
+    print("résultat pour la taille",len(liste_resultat_taille[0]),liste_resultat_taille[1])   
+    liste_resultat_rapport =glouton(liste_instance, 20, rapport)
+    print("résultat pour le rapport",len(liste_resultat_rapport[0]),liste_resultat_rapport[1])
